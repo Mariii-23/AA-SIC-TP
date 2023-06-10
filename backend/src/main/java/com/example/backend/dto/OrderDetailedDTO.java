eb42c0d3e7ea31c9faed6e6b7eafc4cd37260d94
Miguel Ângelo Ferreira Fernandes <miguelffernandes13@gmail.com>
2023-06-10 12:11:18 +0100

    Some DTOs for OrderController requests

---
+++ backend/src/main/java/com/example/backend/dto/OrderDetailedDTO.java
@@ -0,0 +1,23 @@
+package com.example.backend.dto;
+
+
+import com.example.backend.model.Order;
+import com.example.backend.model.OrderItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDetailedDTO extends OrderSimpleDTO {
+    private List<ItemDTO> items = new ArrayList<>();
+
+    public OrderDetailedDTO(Order order){
+        super(order);
+        order.getItems().forEach(orderItem -> {
+            items.add(new ItemDTO(orderItem.getProduct().getName(),
+                                  orderItem.getPrice(),
+                                  orderItem.getQuantity(),
+                                  orderItem.getMaterial().getImage()));
+        });
+    }
+
+}
