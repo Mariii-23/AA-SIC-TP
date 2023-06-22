75bd07804b61a97ea0c177ae858df4750706a8fe
Miguel Ângelo Ferreira Fernandes <miguelffernandes13@gmail.com>
2023-06-22 12:04:42 +0100

    ImageDTO

---
+++ backend/src/main/java/com/example/backend/dto/productDTOs/ImageDTO.java
@@ -0,0 +1,16 @@
+package com.example.backend.dto.productDTOs;
+
+public class ImageDTO {
+    private byte[] image;
+
+    public ImageDTO() {
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
